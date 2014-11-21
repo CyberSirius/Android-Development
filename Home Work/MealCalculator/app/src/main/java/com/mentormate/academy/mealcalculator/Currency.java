fa690dd86e82da15898d3222c91433d998d4984c
CyberSirius <sirius.rashev@gmail.com>
2014-11-21 21:14:29 +0200

    Alpha

---
+++ Home Work/MealCalculator/app/src/main/java/com/mentormate/academy/mealcalculator/Currency.java
@@ -0,0 +1,20 @@
+package com.mentormate.academy.mealcalculator;
+
+public enum Currency{
+    EUR(1, "EUR"),
+    BNG(1.96, "BGN"),
+    USD(1.24, "USD");
+    private double exchangeRate;
+    private String name;
+    private Currency(double exRate, String name){
+        this.exchangeRate=exRate;
+        this.name=name;
+    }
+    public Double Exchange(){
+        return exchangeRate;
+    }
+    @Override
+    public String toString(){
+        return name;
+    }
+}
--- Home Work/MealCalculator/app/src/main/java/com/mentormate/academy/mealcalculator/UserActivity.java
+++ Home Work/MealCalculator/app/src/main/java/com/mentormate/academy/mealcalculator/UserActivity.java
@@ -10,13 +10,19 @@ import android.widget.CheckBox;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
-
 public class UserActivity extends Activity implements View.OnClickListener, SeekBar.OnSeekBarChangeListener {
+    //<editor-fold desc="Constants">
+    static final int DISH_PRICE=5;
+    static final int DESSERT_PRICE=2;
+    static final int DRINK_PRICE=2;
+    double totalPrice=0;
+    //</editor-fold>
+    Currency currentCurrency=Currency.EUR;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
-
+        //<editor-fold desc="Button init">
         Button btnEUR = (Button) findViewById(R.id.btnEUR);
         btnEUR.setOnClickListener(this);
 
@@ -43,16 +49,7 @@ public class UserActivity extends Activity implements View.OnClickListener, Seek
 
         Button btnAbout = (Button) findViewById(R.id.btnAbout);
         btnAbout.setOnClickListener(this);
-
-        SeekBar seekDrink = (SeekBar) findViewById(R.id.seekDrink);
-        seekDrink.setOnSeekBarChangeListener(this);
-
-        CheckBox checkDelivery = (CheckBox) findViewById(R.id.checkHomeDelivery);
-
-        TextView txtTotalPrice = (TextView) findViewById(R.id.txtTotalPrice);
-
-        TextView txtTotalPriceCurrency = (TextView) findViewById(R.id.txtCurrencyTotalPrice);
-
+        //</editor-fold>
 
     }
 
@@ -81,38 +78,81 @@ public class UserActivity extends Activity implements View.OnClickListener, Seek
     @Override
     public void onClick(View v) {
 
+        TextView txtTotalPriceCurrency = (TextView) findViewById(R.id.txtCurrencyTotalPrice);
+
+        TextView txtTotalPrice = (TextView) findViewById(R.id.txtTotalPrice);
         TextView txtDishQuantity = (TextView) findViewById(R.id.txtDishQuantity);
         int dishQuantity = Integer.parseInt(txtDishQuantity.getText().toString());
 
         TextView txtDessertQuantity = (TextView) findViewById(R.id.txtDessertQuantity);
         int dessertQuantity = Integer.parseInt(txtDessertQuantity.getText().toString());
 
+        SeekBar seekDrink = (SeekBar) findViewById(R.id.seekDrink);
+        seekDrink.setOnSeekBarChangeListener(this);
 
+        CheckBox checkDelivery = (CheckBox) findViewById(R.id.checkHomeDelivery);
+
+
+        //<editor-fold desc="Button switch">
         switch (v.getId()) {
+
             case R.id.btnPlusDish:
                 if (dishQuantity < 10) {
                     txtDishQuantity.setText(String.valueOf((dishQuantity + 1)));
                 }
                 break;
+
             case R.id.btnMinusDish:
                 if (dishQuantity > 0) {
                     txtDishQuantity.setText(String.valueOf((dishQuantity - 1)));
                 }
                 break;
-        }
 
-        switch (v.getId()) {
             case R.id.btnPlusDessert:
                 if (dessertQuantity < 10) {
                     txtDessertQuantity.setText(String.valueOf((dessertQuantity + 1)));
                 }
                 break;
+
             case R.id.btnMinusDessert:
                 if (dessertQuantity > 0) {
                     txtDessertQuantity.setText(String.valueOf((dessertQuantity - 1)));
                 }
                 break;
+            case R.id.btnCalculate:
+                double tempPrice=0;
+                if (checkDelivery.isChecked()){
+                    tempPrice+=10;
+                }
+                tempPrice+=dishQuantity*DISH_PRICE + dessertQuantity*DESSERT_PRICE + seekDrink.getProgress()*DRINK_PRICE;
+                totalPrice=tempPrice;
+                totalPrice=CurrencyConverter(Currency.EUR,currentCurrency, totalPrice);
+                txtTotalPrice.setText(String.valueOf(totalPrice));
+                break;
+            case R.id.btnEUR:
+                totalPrice=CurrencyConverter(currentCurrency,Currency.EUR,totalPrice);
+                currentCurrency=Currency.EUR;
+                txtTotalPrice.setText(String.valueOf(totalPrice));
+                txtTotalPriceCurrency.setText(currentCurrency.toString());
+                break;
+            case R.id.btnUSD:
+                totalPrice=CurrencyConverter(currentCurrency,Currency.USD,totalPrice);
+                currentCurrency=Currency.USD;
+                txtTotalPrice.setText(String.valueOf(totalPrice));
+                txtTotalPriceCurrency.setText(currentCurrency.toString());
+                break;
+            case R.id.btnBGN:
+                totalPrice=CurrencyConverter(currentCurrency,Currency.BNG,totalPrice);
+                currentCurrency=Currency.BNG;
+                txtTotalPrice.setText(String.valueOf(totalPrice));
+                txtTotalPriceCurrency.setText(currentCurrency.toString());
+                break;
         }
+        //</editor-fold>
+    }
+
+    private double CurrencyConverter(Currency currentCurrency, Currency futureCurrency, double amount) {
+        return (amount/currentCurrency.Exchange())*futureCurrency.Exchange();
     }
 
     @Override
