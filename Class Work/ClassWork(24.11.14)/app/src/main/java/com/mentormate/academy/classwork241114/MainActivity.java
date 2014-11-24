ce2ad72d97ea2608a4adb2243725e084d164ba32
CyberSirius <sirius.rashev@gmail.com>
2014-11-24 20:08:57 +0200

    Logs and first test added

--- Class Work/ClassWork(24.11.14)/app/src/main/java/com/mentormate/academy/classwork241114/MainActivity.java
+++ Class Work/ClassWork(24.11.14)/app/src/main/java/com/mentormate/academy/classwork241114/MainActivity.java
@@ -2,18 +2,56 @@ package com.mentormate.academy.classwork241114;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
+    private final String TAG = getClass().getSimpleName();
+    static int value;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button incrementValue=(Button) findViewById(R.id.increment_value);
+        incrementValue.setOnClickListener(this);
+        value=0;
+        Log.d(TAG, "onCreate()");
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d(TAG, "onPause()");
+        super.onPause();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d(TAG, "onDestroy()");
+        super.onDestroy();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -35,5 +73,26 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.increment_value: value++;
+                Log.d(TAG, "Variable value is " + value);
+                break;
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle savedData) {
+        savedData.putInt("savedVar", value);
+        super.onSaveInstanceState(savedData);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedData) {
+        super.onRestoreInstanceState(savedData);
+        value=savedData.getInt("savedVar");
     }
 }
